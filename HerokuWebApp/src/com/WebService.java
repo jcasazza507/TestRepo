54c2eb63a17c65c9325983ed55d8ea94afa80042
jcasazza507 <evan@Evans-MacBook-Pro.local>
2016-06-17 16:53:59 -0500

    Changed java.ws.rs scope to provided

--- HerokuWebApp/src/com/WebService.java
+++ HerokuWebApp/src/com/WebService.java
@@ -19,20 +19,20 @@ public class WebService
 	{
 		final int port = System.getenv("PORT") != null ? Integer.valueOf(System.getenv("PORT")) : 8080;
 		final URI baseUri = UriBuilder.fromUri("http://0.0.0.0/").port(port).build(); //do i need to alter this for heroku?
-        final Application application = Application.builder(ResourceConfig.builder().packages(WebService.class.getPackage().getName()).build()).build();
-        application.addModules(new JsonJacksonModule());
-        final HttpServer httpServer = GrizzlyHttpServerFactory.createHttpServer(baseUri, application);
+        	final Application application = Application.builder(ResourceConfig.builder().packages(WebService.class.getPackage().getName()).build()).build();
+        	application.addModules(new JsonJacksonModule());
+		final HttpServer httpServer = GrizzlyHttpServerFactory.createHttpServer(baseUri, application);
         
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-        	@Override
-        	public void run()
-        	{
-        		httpServer.stop();
-        	}
-        });
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+        		@Override
+        		public void run()
+        		{
+        			httpServer.stop();
+        		}
+        	});
         
-        contentUrl = System.getenv("CONTENT_URL") != null ? System.getenv("CONTENT_URL") : CONTENT_PATH;
-        Thread.currentThread().join();
+        	contentUrl = System.getenv("CONTENT_URL") != null ? System.getenv("CONTENT_URL") : CONTENT_PATH;
+        	Thread.currentThread().join();
 	}
 
 }
