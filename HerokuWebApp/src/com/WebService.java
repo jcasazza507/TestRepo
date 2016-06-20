05c5fff1d7f3a647a3592d72277aeca99c6ae50c
jcasazza507 <evan@Evans-MacBook-Pro.local>
2016-06-20 10:26:30 -0500

    Debug
    Trying to debug exceptions thrown. Might have to do with the uri

--- HerokuWebApp/src/com/WebService.java
+++ HerokuWebApp/src/com/WebService.java
@@ -18,7 +18,7 @@ public class WebService
 	public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException 
 	{
 		final int port = System.getenv("PORT") != null ? Integer.valueOf(System.getenv("PORT")) : 8080;
-		final URI baseUri = UriBuilder.fromUri("http://frozen-falls-40487/").port(port).build(); //do i need to alter this for heroku?
+		final URI baseUri = UriBuilder.fromUri("http://0.0.0.0/").port(port).build(); //do i need to alter this for heroku?
         	final Application application = Application.builder(ResourceConfig.builder().packages(WebService.class.getPackage().getName()).build()).build();
         	application.addModules(new JsonJacksonModule());
 		final HttpServer httpServer = GrizzlyHttpServerFactory.createHttpServer(baseUri, application);
